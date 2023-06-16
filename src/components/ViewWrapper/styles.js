5906f4b8f5a692b837053a9a7742de43b2479ae9
Saad Shakeel <saadshakeelyt@gmail.com>
2023-06-16 15:16:18 +0500

    gradientBottomBar

---
+++ src/components/ViewWrapper/styles.js
@@ -0,0 +1,31 @@
+import {vh, vw} from '../../utils/units';
+
+import {StyleSheet, useColorScheme} from 'react-native';
+import {colors, fontColors} from '../../utils/theme';
+
+const lightStyles = StyleSheet.create({
+  container: {
+    height: '100%',
+    paddingHorizontal: vw * 5,
+    backgroundColor: colors.light.primary,
+  },
+});
+
+const darkStyles = StyleSheet.create({
+  container: {
+    height: '100%',
+    paddingHorizontal: vw * 3,
+    backgroundColor: colors.dark.primary,
+  },
+});
+
+const MyStyles = () => {
+  const theme = useColorScheme();
+  if (theme == 'dark') {
+    return darkStyles;
+  } else {
+    return lightStyles;
+  }
+};
+
+export default MyStyles;
