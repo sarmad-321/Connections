a96fa4e4768588a2968239303f9033c16f072ce1
sarmad-321 <sarmadshakeel20@gmail.com>
2023-07-22 17:15:33 +0500

    flatlist profile details

--- src/screens/BottomTabScreens/ProfileDetailScreen/index.js
+++ src/screens/BottomTabScreens/ProfileDetailScreen/index.js
@@ -1,19 +1,20 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useState, useRef} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
 import ScreenWraper from '../../../components/ScreenWrapper';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import ProfileCard from '../../../components/ProfileCard';
 import ShadowView from '../../../components/ShadowView';
 import styles from './styles';
 import Poppins from '../../../components/TextWrapper/Poppins';
 import AbhayaLibre from '../../../components/TextWrapper/AbhayaLibre';
 import LikeIcon from '../../../components/Buttons/LikeIcon';
-import {ScrollView} from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import QuoteCard from '../../../components/QuoteCard';
 import InfoCard from '../../../components/InfoCard';
 import CommentPopup from '../../../components/Popups/CommentPopup';
 
 const ProfileDetailScreen = () => {
+  const data = [1, 2, 3, 4, 5, 6]
   const infoPopup = useRef();
   const HandlePress = () => {
     infoPopup.current.show();
@@ -22,21 +23,32 @@ const ProfileDetailScreen = () => {
   return (
     <ScreenWraper>
       <SafeAreaView>
-        <ScrollView>
-          <ProfileCard like comment onPress={HandlePress} />
-          <QuoteCard
-            text={'Lorem Ipsum is simply dummy text of the printing'}
-            heading={'Getting Rid of dead bodies'}
-            like
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <FlatList
+            data={data}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <>
+                <ProfileCard like comment onPress={HandlePress} />
+                <QuoteCard
+                  text={'Lorem Ipsum is simply dummy text of the printing'}
+                  heading={'Getting Rid of dead bodies'}
+                  like
+                />
+                <CommentPopup
+                  reference={infoPopup}
+                  title={'Blind Date'}
+                  message={
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis vestibulum est, a faucibus.. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis vestibulum est, a faucibus.'
+                  }
+                />
+                <InfoCard />
+              </>
+            )}
           />
-          <CommentPopup
-            reference={infoPopup}
-            title={'Blind Date'}
-            message={
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis vestibulum est, a faucibus.. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis vestibulum est, a faucibus.'
-            }
-          />
-          <InfoCard />
+
+
         </ScrollView>
       </SafeAreaView>
     </ScreenWraper>
