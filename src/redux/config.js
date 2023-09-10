ce3587961d8010d29a7398de00ea30d66f30e7a5
sarmad-321 <sarmadshakeel20@gmail.com>
2023-09-10 20:25:08 +0500

    self signed certificate

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,7 +1,7 @@
 export const urls = {
   v1: `https://average-stole-fox.cyclic.app/api`,
   v2: 'http://143.198.64.59/api',
-  local: 'http://10.1.124.246:4000/api',
+  local: 'https://10.1.124.246:4000/api',
 };
 function trimApi(url) {
   if (url.endsWith('/api')) {
